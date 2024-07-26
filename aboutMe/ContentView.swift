c197aa5a8ef975412b887a116ca383db05ba415b
Mosope <moshiola14@gmail.com>
2024-07-26 13:29:47 +0100

    Initial Commit

---
+++ aboutMe/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  aboutMe
+//
+//  Created by Scholar on 25/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+        
+            
+            Color(red: 179/255, green: 164/255, blue: 255/255).ignoresSafeArea()
+            VStack {
+                Image("jojos")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit).cornerRadius(15)
+                HStack {
+                    Text("Jojo's")
+                        .font(.system(size: 30, weight: .semibold))
+                    Text("Bizarre")
+                        .font(.system(size: 30, weight: .semibold))
+                    Text("Adventure")
+                        .font(.system(size: 27, weight: .semibold))
+                    
+                }
+                Text("Hirohiko Araki")
+                    .font(.system(size: 25, weight: .semibold))
+                Text("The creator, of Jojo's Bizarre Adventure.")
+                Text("Fun fact: ")
+                    .font(.system(size: 20, weight: .semibold))
+                Text("Araki drew his very first manga while")
+                Text("he was in 4th grade.")
+            }.padding().background(Rectangle().foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius:15)
+                .padding()
+          
+            VStack {
+                
+                        Text("  All About Me:").font(.system(size: 40, weight: .bold))
+                    .font(.title)
+                    .foregroundColor(Color.white)
+                
+                        Text("My love for the anime").font(.system(size: 30, weight: .semibold))
+                    .font(.subheadline)
+                    .foregroundColor(Color.white)
+                Text(" Jojo's Bizarre Adventure!").font(.system(size: 30, weight: .semibold))
+                    .font(.subheadline)
+                    .foregroundColor(Color.white)
+                
+                Spacer()
+               
+                    }
+        }
+                
+            }
+        }
+        
+    
+
+
+        
+        #Preview {
+            ContentView()
+        }
+        
+  
